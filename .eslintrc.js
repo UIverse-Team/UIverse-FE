e33bda75651245763147ae972aac20256e255e79
최영애 <choiaeyoung1001@gmail.com>
2025-02-21 19:26:47 +0900

    test: 허스키 커밋 메세지 설정 테스트

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', // Prettier 설정과 통합
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+}
