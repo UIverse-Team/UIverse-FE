2764ccaebec8add986956dcb93f25b4c6d5aaf9e
mgyang53 <mg960503@gmail.com>
2025-03-08 10:37:26 +0900

    fix: 에시 메인페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import TypoSystem from '@/components/design-system/TypoSystem'
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl font-bold mb-8">UIverse 디자인 시스템</h1>
+      <h1 className="text-title1 text-center mb-8">UIverse 디자인 시스템</h1>
 
       <ColorPalette />
 
