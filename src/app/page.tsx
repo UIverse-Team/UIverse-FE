32390becf4194586e977a0421e49676ce930c268
mgyang53 <mg960503@gmail.com>
2025-03-09 17:38:31 +0900

    setting: breakpoint 설정 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import BreakpointTest from '@/components/design-system/BreakpointTest'
 import ColorPalette from '@/components/design-system/ColorPalette'
 import TypoSystem from '@/components/design-system/TypoSystem'
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-title1 text-center mb-8">UIverse 디자인 시스템</h1>
+    <main className="space-y-8">
+      <h1 className="text-title1 text-center">UIverse 디자인 시스템</h1>
+
+      <BreakpointTest />
 
       <ColorPalette />
 
