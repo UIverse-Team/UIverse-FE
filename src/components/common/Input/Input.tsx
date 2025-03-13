6d7694ff70a905e0cd6f870e1cf1c80ed2cd96fa
최영애 <choiaeyoung1001@gmail.com>
2025-03-13 16:46:09 +0900

    feature: input 마크업

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import { cn } from '@/lib/utils'
+import { cva, VariantProps } from 'class-variance-authority'
+
+const InputVariants = cva(
+  'typo-body3 border p-4 rounded-sm outline-none placeholder-assistive text-secondary',
+  {
+    variants: {
+      variant: {
+        default: 'border-assistive',
+        focused: 'border-secondary',
+        error: 'border-error',
+        disabled: 'border-assistive bg-disabled-fill cursor-not-allowed text-disabled',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
+interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof InputVariants> {
+  error?: boolean
+}
+
+function Input({ className, variant, type, disabled, error, ...props }: InputProps) {
+  const [isFocused, setIsFocused] = React.useState(false)
+
+  let currentState = variant
+  if (disabled) {
+    currentState = 'disabled'
+  } else if (isFocused) {
+    currentState = 'focused'
+  } else if (error) {
+    currentState = 'error'
+  }
+
+  return (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(InputVariants({ variant: currentState }), '', className)}
+      disabled={disabled}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      {...props}
+    />
+  )
+}
+
+export { Input }
