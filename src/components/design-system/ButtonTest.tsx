dd724327d9edbd6ecc8da755d6198ab4a45f0f3b
mgyang53 <mg960503@gmail.com>
2025-03-13 22:39:43 +0900

    file: 폴더명 수정

--- src/components/design-system/ButtonTest.tsx
+++ src/components/design-system/ButtonTest.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from '../common/button/Button'
+import { Button } from '../common/Button/Button'
 
 const ButtonTest = () => {
   const variants = ['primary', 'secondary', 'tertiary', 'outline']
