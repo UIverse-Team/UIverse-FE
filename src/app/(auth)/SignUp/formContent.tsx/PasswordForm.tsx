5cb8e942560ae180a04d576ea0737067715004c3
최영애 <choiaeyoung1001@gmail.com>
2025-03-17 22:56:12 +0900

    design: 비밀번호 입력 화면 마크업

--- src/app/(auth)/SignUp/formContent.tsx/PasswordForm.tsx
+++ src/app/(auth)/SignUp/formContent.tsx/PasswordForm.tsx
@@ -2,19 +2,80 @@ import { HelperLabel } from '@/components/common/HelperLabel/HelperLabel'
 import { SignUpFormProps } from '../page'
 import CheckIcon from '/public/check.svg'
 import Button from '@/components/common/button/Button'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
+import { Input } from '@/components/common/Input/Input'
 
 export const PasswordForm = ({ next }: SignUpFormProps) => {
-  const [isCurrentStepValid] = useState(false)
+  const [isCurrentStepValid] = useState(true)
+  const [password, setPassword] = useState('')
+  const [passwordCheck, setPasswordCheck] = useState('')
+  const [lengthStat] = useState<'default' | 'success'>('default')
+  const [numberStat] = useState<'default' | 'success'>('success')
+  const [specialCharStat] = useState<'default' | 'success'>('default')
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setPassword(value)
+  }
+
+  const handlePasswordCheckChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setPasswordCheck(value)
+  }
+
   return (
     <>
-      <label>Password</label>
-      <input />
-      <HelperLabel className="w-[338px] min-h-[29px] px-0.5 py-1.5 text-left" variant="error">
-        <CheckIcon className="w-3 h-3" />
-        {'hi'}
-      </HelperLabel>
-      <Button onClick={next} disabled={!isCurrentStepValid}>
+      <div className="pb-6">
+        <div className="w-full flex items-center justify-between">
+          <label htmlFor="password">비밀번호</label>
+          <Input
+            id="password"
+            variant="auth"
+            placeholder="비밀번호"
+            value={password}
+            type="password"
+            onChange={handlePasswordChange}
+            className="w-[338px]"
+          />
+        </div>
+        <div className="w-full flex justify-end">
+          <div className="flex gap-1.5 w-[338px] min-h-[29px] px-0.5 py-1.5 text-left">
+            <HelperLabel variant={lengthStat}>
+              <CheckIcon className="w-3 h-3" />
+              {'8-20자 이내'}
+            </HelperLabel>
+            <HelperLabel variant={numberStat}>
+              <CheckIcon className="w-3 h-3" />
+              {'특수문자'}
+            </HelperLabel>
+            <HelperLabel variant={specialCharStat}>
+              <CheckIcon className="w-3 h-3" />
+              {'숫자'}
+            </HelperLabel>
+          </div>
+        </div>
+        <div className="w-full flex items-center justify-between">
+          <label htmlFor="password">비밀번호 확인</label>
+          <Input
+            id="passwordCheck"
+            variant="auth"
+            placeholder="비밀번호"
+            value={passwordCheck}
+            type="password"
+            onChange={handlePasswordCheckChange}
+            className="w-[338px]"
+          />
+        </div>
+        <div className="w-full flex justify-end">
+          <div className="flex gap-1.5 w-[338px] min-h-[29px] px-0.5 py-1.5 text-left">
+            <HelperLabel>
+              <CheckIcon className="w-3 h-3" />
+              {'비밀번호 일치'}
+            </HelperLabel>
+          </div>
+        </div>
+      </div>
+      <Button className="mt-2" onClick={next} disabled={!isCurrentStepValid}>
         {'다음'}
       </Button>
     </>
