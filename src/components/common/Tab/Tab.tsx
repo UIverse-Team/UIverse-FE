d504a5cb2498e7400ec5f9e9693f4acb3a9d45fb
mgYang53 <50770004+mgYang53@users.noreply.github.com>
2025-03-19 21:36:54 +0900

    [feature] Tab 컴포넌트 및 스토리 추가 (#54)
    * setting: shadcn tab 컴포넌트 설치
    
    * file: 사용하지 않는 파일 삭제
    
    * feature: tab 컴포넌트 및 스토리 추가

---
+++ src/components/common/Tab/Tab.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import * as React from 'react'
+import * as TabsPrimitive from '@radix-ui/react-tabs'
+import { cn } from '@/lib/utils'
+
+function Tabs({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Root>) {
+  return (
+    <TabsPrimitive.Root
+      data-slot="tabs"
+      className={cn('flex flex-col gap-6', className)}
+      {...props}
+    />
+  )
+}
+
+function TabsList({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List
+      data-slot="tabs-list"
+      className={cn('flex items-center justify-center gap-4', className)}
+      {...props}
+    />
+  )
+}
+
+function TabsTrigger({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      data-slot="tabs-trigger"
+      className={cn(
+        'text-assistive typo-h3 inline-flex flex-1 items-center justify-center px-2 pb-2.5 border-b-2 border-transparent data-[state=active]:border-normal data-[state=active]:text-normal whitespace-nowrap transition-[color,border-color] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 cursor-pointer',
+        className,
+      )}
+      {...props}
+    />
+  )
+}
+
+function TabsContent({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Content>) {
+  return (
+    <TabsPrimitive.Content
+      data-slot="tabs-content"
+      className={cn('flex-1 outline-none', className)}
+      {...props}
+    />
+  )
+}
+
+export { Tabs, TabsList, TabsTrigger, TabsContent }
