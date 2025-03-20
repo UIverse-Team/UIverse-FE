8c0bca11f6ec67a34cc79312c7c3e66192606b39
mgyang53 <mg960503@gmail.com>
2025-03-20 12:25:55 +0900

    feature: 메뉴 관련 상수 및 타입 추가

---
+++ src/constants/menu.ts
@@ -0,0 +1,14 @@
+import { MenuItem } from '@/types/menu/menuType'
+
+export const MENU_ITEMS: MenuItem[] = [
+  { id: 'home', label: '홈', href: '/' },
+  { id: 'recommend', label: '맞춤추천', href: '#' },
+  { id: 'sale', label: '세일', href: '#' },
+  { id: 'best', label: '베스트', href: '#' },
+  { id: 'divider1', label: '', href: '', isDivider: true },
+  { id: 'clothes', label: '의류', href: '#' },
+  { id: 'accessories', label: '패션잡화', href: '#' },
+  { id: 'digital', label: '디지털/가전', href: '#' },
+  { id: 'furniture', label: '가구/인테리어', href: '#' },
+  { id: 'food', label: '식품', href: '#' },
+]
