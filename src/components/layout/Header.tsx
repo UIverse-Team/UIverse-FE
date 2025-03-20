6c33e41be625d7f1e7755d75230c9dc6e0d52282
mgYang53 <50770004+mgYang53@users.noreply.github.com>
2025-03-20 14:03:05 +0900

    [design] 헤더 마크업 1차 완료 (#56)
    * fix: svgr 플러그인 선택적 사용 처리
    
    * design: button 태그 기본 커서 포인터 설정
    
    * feature: 메뉴 관련 상수 및 타입 추가
    
    * design: 헤더 마크업 추가

--- src/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="flex justify-center items-center bg-tertiary py-5">
-      <div className="container">헤더</div>
-    </header>
-  )
-}
-
-export default Header
