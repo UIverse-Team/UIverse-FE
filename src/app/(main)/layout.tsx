df94f852d3a4f4e87f4993e1a6e6b99abbefbfad
mgyang53 <mg960503@gmail.com>
2025-03-21 17:05:44 +0900

    fix: 메인페이지 레이아웃 bg 제거

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import Container from '@/components/layout/Container/Container'
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
-  return <Container>{children}</Container>
+  return <Container isBg={false}>{children}</Container>
 }
