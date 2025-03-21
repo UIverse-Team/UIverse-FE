e1cd95fe965b214fc97768c16ad1be63f708d62e
rondido <rppr001@naver.com>
2025-03-21 11:23:29 +0900

    feature: apis api router로 변경

--- src/apis/cart.ts
+++
@@ -1,6 +0,0 @@
-import HttpClient from './httpClient'
-
-export const deleteCartItems = async (productId: number) => {
-  console.log(productId)
-  await HttpClient.delete(`/guestcarts/${productId}`)
-}
