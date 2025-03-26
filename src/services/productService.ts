527a50ae9b2e06fb3396d38e9780abaf304ac0d1
mgyang53 <mg960503@gmail.com>
2025-03-26 14:44:39 +0900

    refact: 메인페이지 리팩토링

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,4 +1,5 @@
 import httpClient from '@/util/httpClient'
+import axios from 'axios'
 
 export async function getProductDetail(productId: number) {
   try {
@@ -6,5 +7,17 @@ export async function getProductDetail(productId: number) {
     return response.data
   } catch (error) {
     console.error(error)
+  }
+}
+
+export async function getAllProducts() {
+  try {
+    // const response = await httpClient.get('products')
+    // 추후 실제 api 서버 주소로 호출하는 것으르 수정 필요
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_SITE_URL}/api/products`)
+    return response.data
+  } catch (error) {
+    console.error(error)
+    return []
   }
 }
