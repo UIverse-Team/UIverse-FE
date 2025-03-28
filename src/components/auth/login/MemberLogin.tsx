ab45616188fb1ee8897f816f060485989b0031ae
최영애 <choiaeyoung1001@gmail.com>
2025-03-29 00:14:02 +0900

    feature: 아이디 저장 기능 구현

--- src/components/auth/login/MemberLogin.tsx
+++ src/components/auth/login/MemberLogin.tsx
@@ -6,15 +6,33 @@ import { toast } from '@/components/common/Toast/Toast'
 import { ROUTES } from '@/constants/routes'
 import { submitLogin } from '@/serverActions/auth/login/actions'
 import { useAuthStore } from '@/store/user'
+import {
+  getLocalStorageItem,
+  removeLocalStorageItem,
+  setLocalStorageItem,
+} from '@/util/localstorageUtil'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const MemberLogin = () => {
   const router = useRouter()
   const login = useAuthStore((state) => state.login)
 
+  // 이메일 입력 상태 관리
+  const [email, setEmail] = useState('')
+  const [rememberId, setRememberId] = useState(false) // 체크박스 상태 관리
+
   const [state, formAction] = React.useActionState(submitLogin, null)
+
+  // localStorage에서 저장된 이메일 불러오기
+  useEffect(() => {
+    const savedEmail = getLocalStorageItem<string>('savedEmail')
+    if (savedEmail) {
+      setEmail(savedEmail)
+      setRememberId(true) // 체크박스 자동 체크
+    }
+  }, [])
 
   useEffect(() => {
     if (state?.error) {
@@ -31,18 +49,37 @@ export const MemberLogin = () => {
 
     if (state?.user) {
       login(state.user)
+
+      // 아이디 저장 체크 여부에 따라 localStorage 처리
+      if (rememberId) {
+        setLocalStorageItem('savedEmail', email)
+      } else {
+        removeLocalStorageItem('savedEmail')
+      }
     }
-  }, [state, login, router])
+  }, [state, login, router, email, rememberId])
 
   return (
     <>
       <form className="pt-4 pb-2" action={formAction}>
         <div className="h-[75px]">
-          <Input variant="auth" name="email" placeholder="이메일" />
+          <Input
+            variant="auth"
+            name="email"
+            placeholder="이메일"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <Input variant="auth" name="password" type="password" placeholder="비밀번호" />
         <div className="mt-10 p-2 pr-0 flex justify-between">
-          <Checkbox id="saveId" size="lg" label="아이디저장" />
+          <Checkbox
+            id="saveId"
+            size="lg"
+            label="아이디 저장"
+            checked={rememberId}
+            onClick={() => setRememberId(!rememberId)}
+          />
           <div className="flex items-center gap-2">
             <TextButton size="sm" className="text-alternative">
               <Link href={ROUTES.FIND.PW}>비밀번호 찾기</Link>
