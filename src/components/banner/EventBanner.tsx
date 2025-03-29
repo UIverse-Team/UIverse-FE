c25ecf4509a2db681faee84f596f12d6f2135487
mgyang53 <mg960503@gmail.com>
2025-03-29 15:11:01 +0900

    design: 임시 배너 컴포넌트 추가

---
+++ src/components/banner/EventBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventBanner = () => {
+  return (
+    <div className="flex h-70 justify-center items-center bg-gray-300">
+      <span className="typo-title1 text-white">배너입니다</span>
+    </div>
+  )
+}
+
+export default EventBanner
