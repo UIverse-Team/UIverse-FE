c314b70a5ffdba09008bcd30b125e9f3b8c76523
rondido <rppr001@naver.com>
2025-03-29 16:30:10 +0900

    file: 카테고리 네비게이션

---
+++ src/app/categories/[categoryId]/page.tsx
@@ -0,0 +1,12 @@
+export default async function CategoryPage() {
+  //   const { data } = await fetchProducts(searchParams)
+
+  return (
+    <div className="container mx-auto py-8">
+      {/* 카테고리 헤더 */}
+
+      <div>123</div>
+      {/* </Suspense> */}
+    </div>
+  )
+}
