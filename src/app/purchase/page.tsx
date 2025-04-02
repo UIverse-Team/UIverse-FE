f45bed5abc3171960de5e036bf86a770bd393cab
rondido <rppr001@naver.com>
2025-04-02 10:56:47 +0900

    fix: vercel 배포 에러 해결

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { CartHeader } from '@/components/cart/CartHeader'
+import LoadingSpinner from '@/components/common/Loading/LoadingSpinner'
 import { PurchasePayForm } from '@/components/purchase/PurchaseForm'
 import { PurchaseProductsList } from '@/components/purchase/PurchaseProductsList'
 import { PurchaseShoppingInfo } from '@/components/purchase/PurchaseShoppingInfo'
@@ -8,7 +9,7 @@ import { purchaseOrders } from '@/services/purchaseService'
 import { useAuthStore } from '@/stores/user'
 import { cartStorageType, CartType } from '@/types/cart/cartType'
 import { purchaseType } from '@/types/purchase/purchaseType'
-import { useEffect, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 
 const Purchasepage = () => {
   const [purchasepageData, setPurchasepageData] = useState({
@@ -85,27 +86,29 @@ const Purchasepage = () => {
     fetchCartHandleApi()
   }, [isLoggedIn, setCartItems, setCartState])
   return (
-    <div className="py-8 gap-4 flex flex-col">
-      <CartHeader />
-      <div className="flex w-full gap-4">
-        <div className="flex gap-4 w-full flex-col">
-          <PurchaseShoppingInfo
+    <Suspense fallback={<LoadingSpinner />}>
+      <div className="py-8 gap-4 flex flex-col">
+        <CartHeader />
+        <div className="flex w-full gap-4">
+          <div className="flex gap-4 w-full flex-col">
+            <PurchaseShoppingInfo
+              purchasepageData={purchasepageData}
+              setPurchasepageData={setPurchasepageData}
+              setUserDefaultAddress={setUserDefaultAddress}
+              userDefaultress={userDefaultress}
+            />
+            <PurchaseProductsList cartItems={cartItems} />
+          </div>
+          <PurchasePayForm
+            cartListItems={cartItems}
             purchasepageData={purchasepageData}
-            setPurchasepageData={setPurchasepageData}
-            setUserDefaultAddress={setUserDefaultAddress}
-            userDefaultress={userDefaultress}
+            cartState={cartState}
+            setCartItems={setCartItems}
+            userDefaultAddress={userDefaultress}
           />
-          <PurchaseProductsList cartItems={cartItems} />
         </div>
-        <PurchasePayForm
-          cartListItems={cartItems}
-          purchasepageData={purchasepageData}
-          cartState={cartState}
-          setCartItems={setCartItems}
-          userDefaultAddress={userDefaultress}
-        />
       </div>
-    </div>
+    </Suspense>
   )
 }
 
