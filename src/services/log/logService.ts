fc0a245c3e69f5032027667f4f062ffa5a85b1a0
mgYang53 <50770004+mgYang53@users.noreply.github.com>
2025-04-10 17:32:30 +0900

    [feature] 페이지 체류, 상품클릭 로그 수집 기능 구현 (#218)
    * fix: log api route gitignore에 추가
    
    * fix: 비회원 장바구니 상품 조회 api 주소 수정
    
    * feature: 페이지 방문 로그 기능 추가
    
    * setting: storybook 버전 업데이트
    
    * feature: 페이지 방문,이탈로그 기능 구현
    
    * refact: 날짜 관련 유틸함수 dateutils로 이동
    
    * fix: 페이지 체류 로그 오류 수정
    
    * fix: 페이지 체류 로그 컴포넌트 이름 수정
    
    * feature: 상품 클릭 로그 기능 구현
    
    * fix: 빌드 오류 수정

---
+++ src/services/log/logService.ts
@@ -0,0 +1,31 @@
+import logHttpClient from '@/util/logHttpClient'
+import type { PageLeaveLog, PageViewLog, ProductClickLog } from '@/types/log/logTypes'
+
+// 페이지 방문 로그
+export const addPageViewLog = async (addPageViewParams: PageViewLog) => {
+  try {
+    const response = await logHttpClient.post(`/page`, addPageViewParams)
+
+    return response.data
+  } catch (error) {
+    console.warn('Failed to add page view logs:', error)
+  }
+}
+
+// 페이지 이탈 로그
+export const updatePageViewLog = (logId: number, updatePageViewParams: PageLeaveLog) => {
+  // navigator.sendBeacon을 사용하여 비동기 요청이 취소되지 않도록 함
+  const blob = new Blob([JSON.stringify(updatePageViewParams)], { type: 'application/json' })
+  const success = navigator.sendBeacon(`/api/logs/page/${logId}/end`, blob)
+
+  return success
+}
+
+// 상품 클릭 로그
+export const addProductClickLog = (productClickParams: ProductClickLog) => {
+  // navigator.sendBeacon을 사용하여 비동기 요청이 취소되지 않도록 함
+  const blob = new Blob([JSON.stringify(productClickParams)], { type: 'application/json' })
+  const success = navigator.sendBeacon(`/api/logs/product/click`, blob)
+
+  return success
+}
