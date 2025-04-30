c4be03ca5d93812c60c3f73f4efff9b11792035a
mgyang53 <mg960503@gmail.com>
2025-04-30 15:27:22 +0900

    feature: 주문 상품 정보 관련 service 추가

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,10 +1,11 @@
 import { apiGet } from '@/libs/axios/apiMethods'
-import { addQueryParams, createEndpoint } from '@/libs/axios/endPoints'
-import { OrderDetail, OrderResponse } from '@/types/orders/orderType'
+import { addQueryParams, createPathWithParams } from '@/libs/axios/endPoints'
+import type { OrderDetail, OrderProduct, OrderResponse } from '@/types/orders/orderType'
 
 export const ENDPOINTS = {
   ORDERS: '/orders',
   ORDER_BY_ID: '/orders/:id',
+  ORDER_PRODUCT: '/orders/:orderDetailId/item',
   GUEST_ORDER: '/ordersGuest/:orderNumber',
 }
 
@@ -29,15 +30,34 @@ export const getAllOrders = async (
     throw error
   }
 }
-export const getByOrders = async (orderId: string) => {
+export const getOrderDetail = async (orderId: string) => {
   // orderId 매개변수를 받아서 엔드포인트에 포함
-  const endpoint = createEndpoint(`${ENDPOINTS.ORDER_BY_ID}/${orderId}`)
+  const endpoint = createPathWithParams(ENDPOINTS.ORDER_BY_ID, { id: orderId })
 
   try {
     const response = await apiGet<OrderDetail>(endpoint)
     return response.data
   } catch (error) {
     console.error(error instanceof Error ? error.message : '알 수 없는 오류')
+    throw error
+  }
+}
+
+/**
+ * 주문 상품 조회
+ */
+export const getOrderProduct = async (orderDetailId: string): Promise<OrderProduct | undefined> => {
+  if (!orderDetailId) return
+
+  const endpoint = createPathWithParams(ENDPOINTS.ORDER_PRODUCT, { orderDetailId })
+  try {
+    const response = await apiGet<OrderProduct>(endpoint)
+    return response.data
+  } catch (error) {
+    console.error(
+      '주문 상품 조회 실패:',
+      error instanceof Error ? error.message : '알 수 없는 오류',
+    )
     throw error
   }
 }
