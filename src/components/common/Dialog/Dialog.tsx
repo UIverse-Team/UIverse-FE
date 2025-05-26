4395f02a673e1db063c7246b2e8a27eaebb827ae
choiyoungae <choiaeyoung1001@gmail.com>
2025-05-26 19:02:07 +0900

    design: modal z-index 수정

--- src/components/common/Dialog/Dialog.tsx
+++ src/components/common/Dialog/Dialog.tsx
@@ -29,7 +29,7 @@ function DialogOverlay({
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-[rgba(0,0,0,0.55)]',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-[999] bg-[rgba(0,0,0,0.55)]',
         className,
       )}
       {...props}
@@ -49,7 +49,7 @@ function DialogContent({
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-[calc(50%)] max-w-[576px] translate-x-[-50%] translate-y-[-50%] rounded-md p-8 duration-200 bg-white',
+          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-[999] grid w-[calc(50%)] max-w-[576px] translate-x-[-50%] translate-y-[-50%] rounded-md p-8 duration-200 bg-white',
           className,
         )}
         {...props}
